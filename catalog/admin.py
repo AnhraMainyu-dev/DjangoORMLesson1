49432db82459c3b6668a7bfaa7fecfe606792973
AnhraMainyu <maximheilelov@gmail.com>
2026-09-07 16:16:23 +0300

    Add location model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from catalog.models import Location
 
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'coordinates_x')
+
 # Register your models here.
-admin.site.register(Location)
+admin.site.register(Location, LocationAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Location(models.Model):
     title = models.CharField(max_length=50)
-    description_short = models.TextField(max_length=200)
-    description_long = models.TextField(max_length=500)
+    description_short = models.TextField(max_length=500)
+    description_long = models.TextField(max_length=5000)
     coordinates_x = models.FloatField()
     coordinates_y = models.FloatField()
 
