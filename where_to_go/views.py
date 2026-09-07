40b7492b0b5971f0f715c8059afee89b304508b7
AnhraMainyu <maximheilelov@gmail.com>
2026-09-07 15:45:43 +0300

    Add frontend

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 def show_home(request):
-    template = loader.get_template('home.html')
+    template = loader.get_template('index.html')
     context = {}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
