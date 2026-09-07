d3e58e948dcb13162935922291117981f074a5a0
AnhraMainyu <maximheilelov@gmail.com>
2026-09-07 16:19:34 +0300

    Add model for images

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,5 @@
+from tkinter import Image
+
 from django.db import models
 
 
@@ -7,4 +9,9 @@ class Location(models.Model):
     description_long = models.TextField(max_length=5000)
     coordinates_x = models.FloatField()
     coordinates_y = models.FloatField()
+    images = models.ManyToManyField(Image, related_name='locations', blank=True)
 
+class ImageField(models.Model):
+    url = models.URLField()
+    order = models.IntegerField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
