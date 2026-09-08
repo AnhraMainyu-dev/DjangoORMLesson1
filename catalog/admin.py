5338cfddc31c37b62372c093f7f67afb50a82698
AnhraMainyu <maximheilelov@gmail.com>
2026-09-08 16:10:05 +0300

    add inline image model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from catalog.models import Location, Image
+from django.db import models
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('title', 'coordinates_x')
+    inlines = [ImageInline]
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('alt', 'order', 'location')
