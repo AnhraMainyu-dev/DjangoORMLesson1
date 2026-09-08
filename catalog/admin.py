05b355236760f7327c43d686997ad26e54d29b49
AnhraMainyu <maximheilelov@gmail.com>
2026-09-08 16:31:10 +0300

    add images preview in admin tab

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,17 +1,34 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
 from catalog.models import Location, Image
 from django.db import models
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['image_preview']
+    fields = ['file', 'order', 'image_preview']
+
+    def image_preview(self, obj):
+        return format_html('<img src="{}" height={}>',
+                           obj.file.url,
+                           200)
+
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('title', 'coordinates_x')
     inlines = [ImageInline]
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('alt', 'order', 'location')
+    readonly_fields = ['image_preview']
+    list_display = ('file', 'order', 'image_preview')
+
+    def image_preview(self, obj):
+        return format_html('<img src="{}" height={}>',
+                           obj.file.url,
+                           200)
 
 
 # Register your models here.
